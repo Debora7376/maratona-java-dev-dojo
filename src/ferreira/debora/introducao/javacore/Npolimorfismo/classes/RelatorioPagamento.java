36c11bd33120a323afa852f36449245ed5c32f07
debora7376 <devsouza.root@gmail.com>
2021-06-01 14:08:35 -0300

    Polimorfismo parte 2

--- src/ferreira/debora/introducao/javacore/Npolimorfismo/classes/RelatorioPagamento.java
+++ src/ferreira/debora/introducao/javacore/Npolimorfismo/classes/RelatorioPagamento.java
@@ -1,2 +1,32 @@
-package ferreira.debora.introducao.javacore.Npolimorfismo.classes;public class RelatorioPagamento {
+package ferreira.debora.introducao.javacore.Npolimorfismo.classes;
+
+public class RelatorioPagamento {
+//    public void relatorioPagamentoGerente(Gerente gerente){
+//        System.out.println("Gerando relatorio de pagamento para a gerencia");
+//        gerente.calcularPagamento();
+//        System.out.println("Nome: "+gerente.getNome());
+//        System.out.println("Salario desse mês: "+gerente.getSalario());
+//    }
+//    public void relatorioPagamentoVendedor(Vendedor vendedor){
+//        System.out.println("Gerando relatorio de pagamento para os vendedores");
+//        vendedor.calcularPagamento();
+//        System.out.println("Nome: "+vendedor.getNome());
+//        System.out.println("Salario desse mês: "+vendedor.getSalario());
+//    }
+
+    // parametros polimorficos ---------->
+    public void relatorioPagamentoGenerico(Funcionario funcionario) {
+        System.out.println("Relatorio de pagamento");
+        funcionario.calcularPagamento();
+        System.out.println("Nome: " + funcionario.getNome());
+        System.out.println("Salario desse mês: " + funcionario.getSalario());
+        // Duas formas de fazer if com instanceof
+        if (funcionario instanceof Gerente) {
+            Gerente gerente = (Gerente) funcionario;
+            System.out.println("Participacao dos lucro: " + gerente.getPnl());
+        }
+        if (funcionario instanceof Vendedor){
+            System.out.println("Total de vendas: "+ ((Vendedor) funcionario).getTotalVendas());
+        }
+    }
 }
--- src/ferreira/debora/introducao/javacore/Npolimorfismo/testes/PolimorfismoTeste.java
+++ src/ferreira/debora/introducao/javacore/Npolimorfismo/testes/PolimorfismoTeste.java
@@ -1,2 +1,22 @@
-package ferreira.debora.introducao.javacore.Npolimorfismo.testes;public class PolimorfismoTeste {
+package ferreira.debora.introducao.javacore.Npolimorfismo.testes;
+
+import ferreira.debora.introducao.javacore.Npolimorfismo.classes.Funcionario;
+import ferreira.debora.introducao.javacore.Npolimorfismo.classes.Gerente;
+import ferreira.debora.introducao.javacore.Npolimorfismo.classes.RelatorioPagamento;
+import ferreira.debora.introducao.javacore.Npolimorfismo.classes.Vendedor;
+
+public class PolimorfismoTeste {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Choji", 5000, 2000);
+        Vendedor vendedor = new Vendedor("Shino", 2000, 20000);
+        RelatorioPagamento relatorio = new RelatorioPagamento();
+//        relatorio.relatorioPagamentoGerente(gerente);
+//        System.out.println("----------------------------------");
+//        relatorio.relatorioPagamentoVendedor(vendedor);
+
+        relatorio.relatorioPagamentoGenerico(gerente);
+        System.out.println("----------------------------------");
+        relatorio.relatorioPagamentoGenerico(vendedor);
+
+    }
 }
