f6ef61db2e40fd7cbc24b2c7acf0241a92a42eab
debora7376 <devsouza.root@gmail.com>
2021-06-01 10:35:29 -0300

    Classes abstratas parte 1

---
+++ src/ferreira/debora/introducao/javacore/Lclassesabstratas/testes/FuncionarioTeste.java
@@ -0,0 +1,16 @@
+package ferreira.debora.introducao.javacore.Lclassesabstratas.testes;
+
+import ferreira.debora.introducao.javacore.Lclassesabstratas.classes.Funcionario;
+import ferreira.debora.introducao.javacore.Lclassesabstratas.classes.Gerente;
+import ferreira.debora.introducao.javacore.Lclassesabstratas.classes.Vendedor;
+
+public class FuncionarioTeste {
+    public static void main(String[] args) {
+        Vendedor vendedor = new Vendedor("Ino", "20102-8", 1500, 5000);
+        Gerente gerente = new Gerente("Sai", "11192-8", 2000);
+        vendedor.calculaSalario();
+        gerente.calculaSalario();
+        System.out.println(vendedor);
+        System.out.println(gerente);
+    }
+}
